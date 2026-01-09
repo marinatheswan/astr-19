89df2890dd754b4e8448a0e111f42473944d3dab
marinatheswan <secayeta@ucsc.edu>
2026-01-08 22:32:43 -0800

    Add files via upload

---
+++ session1.py
@@ -0,0 +1,9 @@
+name = "Marina Cayetano"
+pronouns = "she\\her"
+favoritemoviechoice = "Inside Out 2"
+favoritefoodchoice = "eels or mushrooms"
+
+print("My full name is: ", name)
+print("My pronouns are:", pronouns)
+print("My favorite movie is", favoritemoviechoice, ". I find Riley to be relatable in several ways. This includes her wanting friends, not being sure what to feel sometimes, and the pressure she puts on herself to do well")
+print("I would choose either", favoritefoodchoice, "as my favorite food. Seafood is usually high on my list, given I am a fan of the ocean. Eels and mushrooms are also umami. My flavor preferences are umami > salty > sweet. In my experience, seafood doesn't go wrong. Neither do mushrooms!")
---
+++ session2.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+x = 5.4
+y = 3.3
+z = x+y
+print(x, "+", y, "=", z, "(sum of two floating point numbers)")
+print(type(z))
+print("Note that the example 4.9+5.1 (which will result in 10.0) is not considered an integer.")
+
+print(".")
+
+
+
+x = 444
+y = 344
+z = x-y
+print(x, "-", y, "=", z, "(difference between two integers)")
+print(type(z))
+
+print(".")
+
+
+
+x = 11.4
+y = 7
+z = x*y
+print(x, "*", y, "=", z, "(float multiplied by integer)")
+print(type(z))
+
+print(".")
